20fbc5079157284bfa8127f7cbba863ff492faa1
Fanz1234 <duffanblank@gmail.com>
2024-03-08 21:21:19 +0700

    FanzBotzzGamonPairing-fix

---
+++ message/sewabot.js
@@ -0,0 +1,29 @@
+
+
+export const dada = (prefix, pushname, ucapanWaktu) => {
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001) 
+return `Hallo, 👋 ${ucapanWaktu} kak
+
+_Silahkan ketik .owner untuk mengetahui pemilik bot ini_`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
